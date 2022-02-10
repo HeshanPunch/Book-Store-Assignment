0b06b88cb564fa0aec02f9def2d986db2f9eeb54
Heshan <heshanpun@gmail.com>
2022-02-09 23:50:25 -0700

    added bookSearch feature

--- src/sait/bms/managers/BooksManager.java
+++ src/sait/bms/managers/BooksManager.java
@@ -9,8 +9,13 @@ public class BooksManager {
 	private ArrayList<Book> books = new ArrayList<>();
 	private static final String PATH = "res/books.txt";
 
+	public BooksManager() throws IOException {
+		LoadBooksFromFile();
+		DisplayMenu();
+
+	}
+
 	public void LoadBooksFromFile() throws IOException {
-		// final int ISBN_NUM_LENTH = 13;
 
 		Scanner in = new Scanner(new File(PATH));
 
@@ -48,32 +53,138 @@ public class BooksManager {
 				System.out.println("Error sorting through input array");
 
 			}
-
-			// only for testing
-			int i = 1;
-			for (Book b : books) {
-
-				if (b instanceof Periodical) {
-					System.out.println(b);
-					System.exit(0);
-				}
-
-			}
-
 		}
-
 	}
 
-	public BooksManager() throws IOException {
-		LoadBooksFromFile();
+	public void DisplayMenu() throws FileNotFoundException {
+		int option = 0;
+		Scanner input = new Scanner(System.in);
 
 		System.out.println("Welcome in ABC Book Company: How May We Assist You?");
 		System.out.println("1\tCheckout Book");
 		System.out.println("2\tFind Books by Title");
 		System.out.println("3\tDisplay Books by Type");
 		System.out.println("4\tProduce Random Book List");
-		System.out.println("5\tSave & Exit");
+		System.out.println("5\tSave & Exit\n");
+		System.out.print("Enter option: ");
+
+		option = input.nextInt();
+
+		while (option != 5) {
+
+			switch (option) {
+
+			case 1: {
+				// System.out.print("Enter ISBN of book: ");
+				// long isbn = input.nextLong();
+				checkoutBook();
+				break;
+			}
+			case 2: {
+				findBook();
+				break;
+			}
+			case 3: {
+				// displayTypeBook();
+				System.out.println("# Type");
+				System.out.println("1	Children's Books");
+				System.out.println("2	Cookbooks");
+				System.out.println("3	Paperbacks");
+				System.out.println("4	Periodicals");
+				int type = input.nextInt();
+				// displayTypeBook(type);
+				break;
+			}
+			case 4: {
+				// randomList();
+				System.out.println("case4");
+				break;
+			}
+			case 5: {
+				saveBooks();
+				System.exit(0);
+				break;
+			}
+			default: {
+				System.out.println("Invalid Input!You should enter a number between 1 and 5.");
+
+			}
+			// input.close();
+
+			}
+		}
 
 	}
 
+	private void checkoutBook() throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter ISBN of book: ");
+		long isbn = input.nextLong();
+
+		for (Book book : books) {
+			if (book.getIsbn() == isbn) {
+				// System.out.println(book.getAvailable() + "before");
+				if (book.getAvailable() > 0) {
+					System.out.println("\nThe book \"" + book.getTitle() + "\" has been checked out.");
+					System.out.println("It can be located using a call number: " + book.getCallNumber() + "\n");
+					book.setAvailable(book.getAvailable() - 1);
+					// System.out.println(book.getAvailable() + "after");
+				} else {
+					System.out.println("\nThe book \"" + book.getTitle() + "\" is not available.\n");
+				}
+			}
+		}
+	}
+
+	public void findBook() throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter title to search for: ");
+		String userInput = input.nextLine().toLowerCase();
+
+		for (Book thisBook : books) {
+			String thisTitle = thisBook.getTitle().toLowerCase();
+			if (thisTitle.contains(userInput)) {
+				System.out.println("Matching books:");
+				System.out.println(thisBook);
+			}
+
+		}
+
+	}
+
+	public void saveBooks() throws FileNotFoundException {
+
+		PrintWriter bookSaver = new PrintWriter(PATH);
+
+		for (Book book : books) {
+
+			if (book instanceof ChildrensBook) {
+
+				bookSaver.write(book.getIsbn() + ";" + book.getCallNumber() + ";" + book.getAvailable() + ";"
+						+ book.getTotal() + ";" + book.getTitle() + ";" + ((ChildrensBook) book).getAuthors() + ";"
+						+ ((ChildrensBook) book).getFormat() + System.lineSeparator());
+
+			} else if (book instanceof CookBook) {
+
+				bookSaver.write(book.getIsbn() + ";" + book.getCallNumber() + ";" + book.getAvailable() + ";"
+						+ book.getTotal() + ";" + book.getTitle() + ";" + ((CookBook) book).getPublisher() + ";"
+						+ ((CookBook) book).getDiet() + System.lineSeparator());
+
+			} else if (book instanceof Paperback) {
+
+				bookSaver.write(book.getIsbn() + ";" + book.getCallNumber() + ";" + book.getAvailable() + ";"
+						+ book.getTotal() + ";" + book.getTitle() + ";" + ((Paperback) book).getAuthors() + ";"
+						+ ((Paperback) book).getYear() + ";" + ((Paperback) book).getGenre() + System.lineSeparator());
+
+			} else if (book instanceof Periodical) {
+
+				bookSaver.write(book.getIsbn() + ";" + book.getCallNumber() + ";" + book.getAvailable() + ";"
+						+ book.getTotal() + ";" + book.getTitle() + ";" + ((Periodical) book).getFrequency()
+						+ System.lineSeparator());
+
+			}
+		}
+		bookSaver.close();
+
+	}
 }
